a9687fd14d620d1321f9045ec21b41c22552ebe2
Bianca Williams <bsewilliams@yahoo.com>
2018-05-12 09:46:59 -0400

    working

---
+++ app/data/friends.js
@@ -0,0 +1,63 @@
+var friends = [
+    {
+        name: "Micheal",
+        photo: "https://www.mercurynews.com/wp-content/uploads/2018/03/jordan.jpg?w=660",
+        scores: ["4", "2", "4", "1", "5", "3", "1", "4", "2", "3"]
+    },
+    {
+        name: "Tiffany",
+        photo: "https://ia.media-imdb.com/images/M/MV5BMjE1NjcxMjIwMl5BMl5BanBnXkFtZTgwOTE3NjIwMzI@._V1_UY317_CR26,0,214,317_AL_.jpg",
+        scores: ["2", "4", "5", "4", "5", "3", "2", "5", "1", "4"]
+},
+    {
+        name: "Bradley",
+        photo: "http://aroundmovies.com/wp-content/uploads/2014/12/Bradley-Cooper-300x342.jpg",
+        scores: ["5", "5", "5", "1", "3", "1", "3", "2", "2", "3"]
+    },
+    {
+        name: "Kate",
+        photo: "http://www2.pictures.stylebistro.com/gi/Kate+Middleton+Long+Hairstyles+Long+Curls+cpymeEQSTIvl.jpg",
+        scores: ["2", "4", "3", "2", "5", "2", "4", "4", "2", "2"]
+    },
+    {
+        name: "Meghan",
+        photo: "https://www.biography.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_700/MTQzMjU0NzQ0Nzk2NzY4MTA5/meghan_markle_photo_brad_barket_gettyimages_516165942jpg.webp",
+        scores: ["5", "5", "3", "1", "4", "5", "5", "3", "2", "3"]
+    },
+    {
+        name: "Gabrielle",
+        photo: "https://ia.media-imdb.com/images/M/MV5BMTkwNjUzMDM2Nl5BMl5BanBnXkFtZTcwNDgyMDY4Mg@@._V1_UY317_CR27,0,214,317_AL_.jpg",
+        scores: ["1", "5", "3", "5", "4", "3", "5", "5", "2", "3"]
+    },
+    {
+        name: "Issa",
+        photo: "https://ia.media-imdb.com/images/M/MV5BMTUyODI3MDg1OF5BMl5BanBnXkFtZTgwMjc5NTc5MDI@._V1_UY317_CR12,0,214,317_AL_.jpg",
+        scores: [
+            "2", "5", "4", "5", "5", "3", "3", "2", "4", "1"
+        ]
+    },
+    {
+        name: "Brad",
+        photo: "https://ia.media-imdb.com/images/M/MV5BMjA1MjE2MTQ2MV5BMl5BanBnXkFtZTcwMjE5MDY0Nw@@._V1_UX214_CR0,0,214,317_AL_.jpg",
+        scores: [
+            "3", "5", "2", "1", "4", "5", "3", "4", "2", "5"
+        ]
+    },
+    {
+        name: "Zoe",
+        photo: "https://ia.media-imdb.com/images/M/MV5BMjA4NDk1NTA1OV5BMl5BanBnXkFtZTcwMTIzMjQ4Ng@@._V1_UY317_CR8,0,214,317_AL_.jpg",
+        scores: [
+            "3", "4", "2", "5", "5", "1", "4", "3", "4", "1"
+        ]
+    },
+    {
+        name: "Eddie",
+        photo: "https://ia.media-imdb.com/images/M/MV5BMTc0NDQzODAwNF5BMl5BanBnXkFtZTYwMzUzNTk3._V1_UY317_CR6,0,214,317_AL_.jpg",
+        scores: [
+            "5", "3", "1", "3", "4", "5", "2", "5", "1", "2"
+        ]
+    },
+
+];
+
+module.exports = friends;
